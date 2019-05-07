36eb213d0f98345bec8406a92f9f567c125d5865
GuilhermeHaetinger <ghaetinger@gmail.com>
2019-05-07 17:49:30 -0300

    added blur script

---
+++ lock_and_blur.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# set the icon and a temporary location for the screenshot to be stored
+icon="$HOME/images/lock-icon-light.png"
+tmpbg='/tmp/screen.png'
+
+# take a screenshot
+scrot "$tmpbg"
+
+# blur the screenshot by resizing and scaling back up
+convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
+
+# overlay the icon onto the screenshot
+convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
+
+# lock the screen with the blurred screenshot
+i3lock -i "$tmpbg"
